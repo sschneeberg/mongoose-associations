304460d2621d0eac16ce608395850e4f8640ff54
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-15 16:57:20 -0500

    Add post routes

--- index.js
+++ index.js
@@ -2,15 +2,38 @@ require('dotenv').config();
 const { urlencoded } = require('express');
 const express = require('express');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-const db = mongoose.connection;
+const BlogPost = require('./models/blog');
 
 app.get('/', (req, res) => {
     res.send('hello');
+});
+
+app.get('/blog', (req, res) => {
+    BlogPost.find().then((found) => {
+        res.status(200).json({ posts: found });
+    });
+});
+
+app.post('/blog', (req, res) => {
+    BlogPost.create(req.body).then((post) => {
+        res.status(201).json({ post });
+    });
+});
+
+app.post('/comment', (req, res) => {
+    BlogPost.findOne().then((post) => {
+        post.comments.push(req.body);
+        res.status(201).json({ comment: req.body, post: post });
+    });
 });
 
 app.listen(process.env.PORT || 3000, console.log('starting server'));
